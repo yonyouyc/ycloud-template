9d8b327df0b5187fffb4abbda0da72daba1884a0
windknow <songhlc@yonyou.com>
2018-02-08 14:37:11 +0800

    修改uglifyjs的依赖

--- template/config/webpack.prod.config.js
+++ template/config/webpack.prod.config.js
@@ -6,7 +6,8 @@ var uglifyjs = require('uglifyjs-webpack-plugin')
 var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 // 去除重复的css
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var ParallelUglifyPlugin =  require('webpack-parallel-uglify-plugin')
 // ...
 config = merge(config, {
   entry: {
@@ -38,17 +39,16 @@ config = merge(config, {
         verbose:  true,        　　　　　　　　　　//开启在控制台输出信息
         dry:      false        　　　　　　　　　　//启用删除文件
       }),
-    new webpack.optimize.UglifyJsPlugin({
-      beautify: false,
-      mangle: {
-        screw_ie8: true,
-        keep_fnames: true
-      },
-      compress: {
-        screw_ie8: true
-      },
-      comments: false,
-      sourceMap: true
+    new ParallelUglifyPlugin({
+      cacheDir: '.cache/',
+      uglifyJS:{
+        output: {
+          comments: false
+        },
+        compress: {
+          warnings: false
+        }
+      }
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
